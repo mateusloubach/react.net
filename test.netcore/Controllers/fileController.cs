8b6042e678f9fefbdcb9e60cf63755204348e656
mateus.loubach <mateus.loubach@valenet.com.br>
2022-03-28 16:09:52 -0300

    first commit

---
+++ test.netcore/Controllers/fileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test.netcore.Model;
+
+namespace file.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class fileController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult Post([FromForm] fileModel file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.fileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.formFile.CopyTo(stream);
+
+                    string filetype = Path.GetExtension(file.fileName);
+
+                    if (filetype != ".csv")
+                        return BadRequest("file type not accepted.");
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status202Accepted);
+                    }
+                }   
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+    }
+}
---
+++ test.netcore/Model/fileModel.cs
@@ -0,0 +1,13 @@
+﻿namespace test.netcore.Model
+{
+    public class fileModel
+    {
+        public string fileName { get; set; }
+        public IFormFile formFile { get; set; }
+        public string fileType { get; set; }
+
+        //IF I were to insert request for MULTIPLE FILES, the code would be:
+        //public List<IFormFile> formFiles { get; set; }
+
+    }
+}
